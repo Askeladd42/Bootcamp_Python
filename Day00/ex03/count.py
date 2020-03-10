216777cf223670810c4cdcb64408fb0eaf20d24c
Askeladd42 <piema.lam@gmail.com>
2020-03-10 15:34:09 +0100

    first commit

--- Day00/ex03/count.py
+++ Day00/ex03/count.py
@@ -6,38 +6,36 @@
 #    By: plam <plam@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/09 19:59:10 by plam              #+#    #+#              #
-#    Updated: 2020/03/10 13:44:30 by plam             ###   ########.fr        #
+#    Updated: 2020/03/10 15:33:51 by plam             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
+from inspect import signature
 
 def text_analyzer(text):
 	"""This a very useful function to detail the number of each type of 
 		character (even spaces) used on it.
 		No that is not a line for slacking!
 	"""
-
-	if not text:
-		print("What is the text to analyse?")
-		text_analyzer(sys.stdin)
-	else:
+	try:
 		lcase = 0
 		ucase = 0
 		p_marks = 0
 		sp = 0
 
 		t_len = len(text)
-		for i in range(t_len):
-			if text[i] in "abcdefghijklmnopqrstuvwxyz":
-				lcase += 1
-			elif text[i] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-				ucase += 1
-			elif text[i] in ".:;?!,":
-				p_marks += 1
-			elif text[i] == " ":
-				sp += 1
-			i += 1
+		if t_len > 0:
+			for i in range(t_len):
+				if text[i] in "abcdefghijklmnopqrstuvwxyz":
+					lcase += 1
+				elif text[i] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+					ucase += 1
+				elif text[i] in ".:;?!,":
+					p_marks += 1
+				elif text[i] == " ":
+					sp += 1
+				i += 1
 		print("The text contains " + str(i) + " characters:")
 		if ucase > 0:
 			print("- " + str(ucase) + " upper letters")
@@ -47,3 +45,6 @@ def text_analyzer(text):
 			print("- " + str(p_marks) + " punctuation marks")
 		if sp > 0:
 			print("- " + str(sp) + " spaces")
+	except TypeError:
+		text = input("What is the text to analyse?")
+		text_analyzer(text)
