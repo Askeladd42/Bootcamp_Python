2ef158a3b1450ef017a1af0cbf75faceaedbb831
Askeladd42 <piema.lam@gmail.com>
2020-03-11 01:27:23 +0100

    first commit

--- Day00/ex05/kata00.py
+++ Day00/ex05/kata00.py
@@ -6,11 +6,15 @@
 #    By: plam <plam@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/10 17:59:40 by plam              #+#    #+#              #
-#    Updated: 2020/03/10 19:25:46 by plam             ###   ########.fr        #
+#    Updated: 2020/03/11 01:27:14 by plam             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 t = (19, 42, 21)
 s_len = len(t)
-s_t = str(t[i] for i in range (s_len))
-print("The " + str(s_len) + " numbers are : " + str(t[s_len-1]))
+s1 = "The " + str(s_len) + " numbers are : "
+for i in range (s_len):
+	s1 += str(t[i])
+	if i < s_len-1:
+		s1 += ", "
+print(s1)
