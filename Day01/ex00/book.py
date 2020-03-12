e08c49018294fc119776ae5ef1aedafb9a68ecd3
Pierre-marie LAM <plam@e1r9p20.42.fr>
2020-03-12 20:44:28 +0100

    first deposit

--- Day01/ex00/book.py
+++ Day01/ex00/book.py
@@ -6,15 +6,17 @@
 #    By: plam <plam@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/12 18:07:19 by plam              #+#    #+#              #
-#    Updated: 2020/03/12 20:17:16 by plam             ###   ########.fr        #
+#    Updated: 2020/03/12 20:39:02 by plam             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+from datetime import datetime
 
 class Book(object):
 	def __init__(self, name, r_t):
 		self.name = name		# class variable unique to each instance
-		self.last_update = 0 #[datetime](https://docs.python.org/3/library/datetime.html)
-		self.creation_date = 0 #[datetime](https://docs.python.org/3/library/datetime.html)
+		self.last_update = datetime.now #[datetime](https://docs.python.org/3/library/datetime.html)
+		self.creation_date = datetime.now #[datetime](https://docs.python.org/3/library/datetime.html)
 		self.recipe_type = r_t #a dictionnary with 3 keys: "starter", "lunch", "dessert". (dict)
 
 	def get_recipe_by_name(self, name):
--- Day01/ex00/test.py
+++ Day01/ex00/test.py
@@ -6,14 +6,17 @@
 #    By: plam <plam@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/12 18:07:25 by plam              #+#    #+#              #
-#    Updated: 2020/03/12 20:23:21 by plam             ###   ########.fr        #
+#    Updated: 2020/03/12 20:44:06 by plam             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#from book import Book
+from book import Book
 from recipe import Recipe
 
 salad_r = Recipe("salad", 1, 15, "starter")
 print(salad_r.name, salad_r.cooking_level, salad_r.cooking_time, salad_r.recipe_type, sep=", ")
 to_print = str(salad_r)
 print(to_print)
+
+raclette = Book("raclette", "dessert")
+print(str(raclette.creation_date))
