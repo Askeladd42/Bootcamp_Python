415df3fd17de6c9545f0c898fdc89c88b758964c
Pierre-marie LAM <plam@e1r9p20.42.fr>
2020-03-12 20:23:40 +0100

    first deposit

--- Day01/ex00/test.py
+++ Day01/ex00/test.py
@@ -6,7 +6,7 @@
 #    By: plam <plam@student.42.fr>                  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/12 18:07:25 by plam              #+#    #+#              #
-#    Updated: 2020/03/12 20:19:12 by plam             ###   ########.fr        #
+#    Updated: 2020/03/12 20:23:21 by plam             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,3 +15,5 @@ from recipe import Recipe
 
 salad_r = Recipe("salad", 1, 15, "starter")
 print(salad_r.name, salad_r.cooking_level, salad_r.cooking_time, salad_r.recipe_type, sep=", ")
+to_print = str(salad_r)
+print(to_print)
